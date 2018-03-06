da82fba0e0c82215c20a0fa912fc6efe50b834b0
Anderson Santos <adsantos@gmail.com>
2018-03-06 08:52:47 -0300

    change env to link

--- bin/run-minio.sh
+++ bin/run-minio.sh
@@ -1,6 +1,6 @@
 docker run --rm -p 9000:9000 --name minio-srv \
---add-host=thumb-srv:172.20.8.203 \
---add-host=rabbit-srv:172.20.8.203 \
+--add-host=fn-srv:172.20.8.203 \
+--link rabbit-srv \
 -v $PWD/srv/data:/data \
 -v $PWD/srv/config:/root/.minio \
 minio/minio server /data
